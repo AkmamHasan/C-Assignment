4e68acfbd579a146346c3a91c7d2efea74fe8c03
Akmam Hasan <62193533+AkmamHasan@users.noreply.github.com>
2020-11-09 18:05:26 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,51 +2,40 @@
 
 using namespace std;
 
-//  Task 2: Using the new operator, allocate a two dimensional character array. Again the number of rows and columns
-//  are going to be provided by the user as input. All of the rows are the same size. Take character strings as input from
-//  the user and then print the strings. Finally de-allocate the array using the delete operator.
+//   Task 1: Using the new operator, allocate an integer array of user specified size (the user gives the size of the array as
+//   input). Assign values to the array elements by taking user inputs and then print the values. Finally de-allocate the array
+//   using the delete operator.
 
 int main()
 {
-    int x,y; char z;
-    cout<<"What is the row of the array? ";
+//cout << "Hello world!" << endl;
+    int x,y;
+    cout<<"What is the size of the array? ";
     cin >>x;
-    cout<<"\nWhat is the column of the array? ";
-    cin>>y;
+    cout<<"The size of the array is "<<x;
 
-    char** dArr = new char*[x]; // this loop allocate 2d array in memory
-    for(int i=0; i<x; i++){
-        dArr[i] = new char[y]; // now it works as dArr[x][y]
-    }
+    int* dArr;            //  allocating memory for array.
+    dArr = new int[x];
 
-    for(int i=0;i<x;i++){      // This loop is used for taking array input
-        for(int j=0; j<y; j++){
+    for(int i=0;i<x;i++){   // This loop is used for taking array input
             cout<<"\nYour Input : ";
-            cin >>z;
-            dArr[i][j]=z;
-        }
+            cin >>y;
+            dArr[i]=y;
     }
 
-    for(int i=0;i<x;i++){
-            for(int j=0; j<y; j++)
-        {                           // this loop is for printing 2d array
-        cout<<dArr[i][j]<<" ";
-        }
-        cout<<endl;
+    for(int i=0;i<x;i++)
+    {                           // this loop is for printing array
+        cout<<dArr[i]<<endl;
     }
 
-    for(int i=0; i<x; i++){  // this loop is for de-allocate memory using delete operator.
-        delete[] dArr[i];
-        delete[] dArr;
-    }
+    delete[] dArr;  // delete operator is used for de-allocating memory.
 
     dArr = NULL;
-    cout<<dArr[0][0]<<endl;
+    cout<<dArr[0]<<endl;
+
 
     return 0;
 }
 
-// Akmam Hasan
-// ID- 1911192642
-
-
+//Akmam Hasan
+//ID-1911192642
